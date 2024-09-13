271d2dd762bd8e3352c7c796713e2051a25b01c7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-13 21:59:42 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -24,3 +24,7 @@ async def react_to_message(client: Client, message: Message):
     
     emoji = random.choice(reactions)
     await message.react(emoji)
+
+@app.on_message(filters.command(["start", "help"]))
+async def react_to_message(client: Client, message: Message):
+    await message.reply_text("I am auto reaction bot. Add me in group or channels for auto react on your post")
