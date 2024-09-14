1f3de294111cdc89a6d9d08118a260156429a740
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-14 15:22:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,5 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_ID = getenv("API_ID", "")
+
+API_HASH = getenv("API_HASH", "")
+
 BOT_TOKENS = [token.strip() for token in getenv('BOT_TOKENS', '').split(',') if token.strip()]
 
