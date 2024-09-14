f74ed2bd872a87a0c343c62cb5f5132afecbbead
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-14 16:10:19 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -4,6 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from VIPMUSIC import app
 # List of reactions
+"""
 reactions = [
     "👍", "👎", "❤️", "🔥", "🥳", "👏", "😁", "😂", "😲", "😱", 
     "😢", "😭", "🎉", "😇", "😍", "😅", "💩", "🙏", "🤝", "🍓", 
@@ -47,3 +48,28 @@ async def react_to_message(client: Client, message: Message):
 @Client.on_message(filters.command(["start", "help"]))
 async def start_help_command(client: Client, message: Message):
     await message.reply_text("I am an auto reaction bot. Add me in group or channels for auto react on your posts.")
+"""
+
+
+
+
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from VIPMUSIC import app
+
+# List of reactions
+reactions = ["👍", "👎", "❤️", "😂", "🤯", "😮", "🤔", "😢"]
+
+
+
+@app.on_message(filters.text)
+async def react_to_message(client: Client, message: Message):
+    """
+    This function reacts to any message with a random reaction from the 'reactions' list.
+    """
+    
+    emoji = random.choice(reactions)
+    await message.react(emoji)
